76c602213a9706d4844fe059f3e37fd7866fa228
Emmiiez <emmiiez.m@gmail.com>
2024-07-02 23:41:12 +0700

    Update ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,14 +1,25 @@
 // Exercise #5
-let getJohnProfile = () => {
-  return new Promise(function (_, reject) {
-    setTimeout(
-      () =>
-        reject({
-          errorCode: 500,
-          message: "👿 Failed to request data from server",
-        }),
-      2000
-    );
-  });
-};
-// Start coding here
+let getJohnProfile = async() => {
+    return new Promise(function (_, reject) {
+      setTimeout(
+        () =>
+          reject({
+            errorCode: 500,
+            message: "👿 Failed to request data from server",
+          }),
+        2000
+      );
+    });
+  };
+  // Start coding here
+
+async function displayProfile (){
+    try {
+        const displayResolved = await getJohnProfile();
+        console.log(displayResolved);
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+displayProfile()
